8c8b6fa22c9cab4feb4699136520584f83923e7b
dangthai121200 <dangthai121200@gmail.com>
2022-03-14 19:23:31 +0700

    add spinner in coponent formlogin

--- src/component/FormLogin/FormLogin.js
+++ src/component/FormLogin/FormLogin.js
@@ -27,25 +27,32 @@ const FormLogin = () => {
    }
    useSelector(state=>state.User.loading === HTTP_STATUS.FULFILLED ? navigate("/"):'')
     return (
-      <div class="container login-container" >
-                <div class="col-md-6 login-form-1">
+      <div className="container login-container" >
+                <div className="col-md-6 login-form-1">
                     <h3>Chào mừng bạn đến với laptopso1vn</h3>
                     <form onSubmit={e=>onSubmit(e)}>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Tên tài khoản *" value={username||""} onChange={e=>setUsername(e.target.value)} />
+                        <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Tên tài khoản *" value={username||""} onChange={e=>setUsername(e.target.value)} />
                         </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Mật khẩu *" value={password||""} onChange={e=>setPassword(e.target.value)} />
+                        <div className="form-group">
+                            <input type="password" className="form-control" placeholder="Mật khẩu *" value={password||""} onChange={e=>setPassword(e.target.value)} />
                         </div>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Đăng nhập" />
+                        <div className="form-group">
+                            <input type="submit" className="btnSubmit" value="Đăng nhập" />
                         </div>
-                        <div class="form-group">
+                        <div className="form-group">
                         <Link to="forgetPassword">Quên mật khẩu ?</Link>
                         </div>
-                        <div class="form-group">
+                        <div className="form-group">
                         <p>Chưa có tài khoản ? <Link to="register">Tạo tài khoản mới</Link></p>
                         </div>
+                        {/* spinner */}
+                        { useSelector(state=>state.User.loading === HTTP_STATUS.PENDING ? 
+                          <button className="btn btn-primary">
+                            <span className="spinner-border spinner-border-sm"></span>
+                            Loading..
+                        </button> :'')}
+                           {/* end spinner */}
                         <h2 style={{color:"red"}}>{useSelector(state=>state.User.loading === HTTP_STATUS.REJECTED?"Sai tên đăng nhập hoặc mật khẩu":"")}</h2>
                     </form>
                 </div>
