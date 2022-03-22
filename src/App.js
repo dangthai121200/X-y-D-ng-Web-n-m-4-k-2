e38266d4abeef8317e6ff8b03feb367dc5e02448
TranBaoPhuc <phuc423vn@gmail.com>
2022-03-22 08:46:39 +0700

    Add AccountInfomation to Website

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import ContactPage from './pages/Contact';
 import About from './pages/About';
 import Cart from './pages/Cart';
 import Pay from './pages/Pay';
-// import AccountInfo from './pages/AccountInfo';
+import AccountInfo from './pages/AccountInfo';
 // import AddFeedBack from './pages/AddFeedBack';
 // import PurchaseHistory from './pages/PurchaseHistory';
 // import OrderDetail from './pages/OrderDetail';
@@ -32,6 +32,7 @@ const App = () => {
       <Route exact path='/Contact' element={<ContactPage/>}/>
       <Route exact path='/Aboutus' element={<About/>}/>
       <Route exact path='/Cart' element={<Cart/>}/>
+      <Route exact path='/AccountInfo' element={<AccountInfo  />}/>
 
       </Routes>
     </BrowserRouter>
